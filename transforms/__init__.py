7b3438ab1c862d1bf0cb638d0b30161cc42fda8c
Owen-Dechow <owen,dechow@gmail.com>
2024-05-31 20:14:11 -0400

    fix __init__

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -1,3 +1,3 @@
-from base_transform import BaseTransform
-from transform_policy import TransformPolicy
-from transforms import StretchY, PointDelta, ShrinkOverLife
+from .base_transform import BaseTransform
+from .transform_policy import TransformPolicy
+from .transforms import StretchY, PointDelta, ShrinkOverLife
